296e1df1947edbfb5731f6f94df1a67ff539dda8
Trung Bui <bmtrungvp@gmail.com>
2025-01-17 09:39:18 +0900

    fix ssh

--- build_container_kcare22_ssh.sh
+++ build_container_kcare22_ssh.sh
@@ -1,6 +1,6 @@
-DOCKERFILE=Dockerfile22
-IMAGE_NAME=mtbui2010/ubuntu22.04:cuda11.7-ros.humble-kcare
-CONTAINER_NAME=kcare22
+DOCKERFILE=Dockerfile22ssh
+IMAGE_NAME=mtbui2010/ubuntu22.04:cuda11.7-ros.humble-ssh
+CONTAINER_NAME=kcare22ssh
 SHARE_DIR=/media/keti/workdir/projects
 
 sudo docker build --tag $IMAGE_NAME  - < $DOCKERFILE
@@ -8,12 +8,14 @@ sudo docker build --tag $IMAGE_NAME  - < $DOCKERFILE
 sudo docker run \
 --name $CONTAINER_NAME \
 -it \
+-d \
 --gpus all \
---net host \
 --privileged \
 --env="DISPLAY=:0.0" \
 -v=/tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v=/dev:/dev \
 -v=$SHARE_DIR:$SHARE_DIR \
 -w=$SHARE_DIR \
+-p 2222:22 \
 $IMAGE_NAME
+#--net host \
